8170abaf8705192da3ba3743e0bfc7d7e34d4b8b
Patrik Trefil <patrik.trefil@gmail.com>
2023-03-09 12:00:53 +0100

    subparsers

--- examples/Program.cs
+++ examples/Program.cs
@@ -1,15 +1,17 @@
-﻿namespace Argsparse.Examples;
+﻿using System;
+
+namespace Argsparse.Examples;
 
 class Program
 {
 
-    internal class ExampleConfiguration
+    internal record ExampleConfiguration
     {
         public string? algorithm;
         public bool help = false;
     }
 
-    static void Main(string[] args)
+    static void SimpleExample(string[] args)
     {
         var config = new ExampleConfiguration();
 
@@ -38,5 +40,52 @@ class Program
         parser.AddOption(algorithmOption);
 
         parser.Parse(args);
+    }
+    internal record ComplexExampleCommandConfig
+    {
+        public bool help = false;
+    }
+    internal record ComplexExampleSubCommandConfig
+    {
+        public bool help = false;
+    }
+
+    static void ComplexExample(string[] args)
+    {
+        var toplevelParser = new Parser<ComplexExampleCommandConfig>(
+            () => new ComplexExampleCommandConfig()
+            )
+        {
+            Name = "My program",
+            Description = "My description",
+            Run = (c) => { Console.WriteLine("I will run after my config is ready"); }
+        };
+        toplevelParser.AddFlag(new Flag<ComplexExampleCommandConfig>
+        {
+            Names = new string[] { "-h", "--help" },
+            Description = "Print help",
+            Action = (c) => { c.help = true; }
+        });
+        var subcommandParser = new Parser<ComplexExampleSubCommandConfig>(
+            () => new ComplexExampleSubCommandConfig()
+            )
+        {
+            Name = "My subcommand",
+            Description = "My subcommand description",
+            Run = (c) => { Console.WriteLine("I will run after my config is ready"); }
+        };
+        subcommandParser.AddFlag(new Flag<ComplexExampleSubCommandConfig>()
+        {
+            Names = new string[] { "-h", "--help" },
+            Description = "Print help"
+        });
+        toplevelParser.AddSubparser("subcommand", subcommandParser);
+        toplevelParser.ParseAndRun(args);
+    }
+
+    static void Main(string[] args)
+    {
+        SimpleExample(args);
+        ComplexExample(args);
     }
 }
