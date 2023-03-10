3d96d08f13898334cbd432439dae0cdf2810d538
zdenecek <zdnek.tomis@gmail.com>
2023-03-10 21:26:43 +0100

    clean up parser file

--- argsparse/Parser.cs
+++ argsparse/Parser.cs
@@ -5,59 +5,47 @@ using System.Linq;
 
 namespace Argsparse;
 
-// TODO: maybe suppor subarsers again? by providing Run delegate
-
-/*
-Documentation
-
-Option a Flag jsou dvě různé úplně nezávislé třídy.
-Option existuje pouze jeden a nemůže se od něj dědit.
-Pokud chceme vyrobit novou Option s předdefinovaným chováním (např. konverzí)
-tak použijeme OptionFactory (vyhneme se tím dedičnosti, což věci zjednodušuje).
-
-Všechny třídy jsou mutable a podporují fluent syntax pomocí klíčového slova
-with. Všechny třídy jsou vždy validní instance.
-
-Fluent syntax je pouze jedna možnost jak třídy používat. Je možno
-vše vyplňovat ručně např. pomocí object initializeru. Zároveň to umožňuje uživateli číst konfiguraci.
-
-Obj. initializer příklad:
-
-```
-var flag = new Flag<Config>
-{
-    Names = new[] { "-h", "--help" },
-    Description = "Print help",
-    Action = (config, value) => { config.PrintHelp = true; }
-};
-```
-
-Tisk helpu je možné zavolat přímo na parseru, ale logika tisku je
-v samostatné třídě. Poskytujeme defaultní implementaci, ale
-uživatel si může napsat vlastní tím že implementuje interface ParserHelpFormatter
-
-Jednotlivé parsery mohou ukládat data do instance na kterou dostane ukazatel
-nebo dostane factory metodu a instanci si vyrobí sám pokud ji bude potřebovat
-(umožňuje se vyhnout spoustě null hodnotám - viz příklad níže)
-
-Používám recordy, protože nám zadarmo dává print metodu, copy ctor, hashing, eq comparison.
-*/
-
+/// <summary>
+/// Models information about a plain argument, how many parts it should consist of.
+/// <para>
+/// See <see cref="Argument{C, V}"/>, <see cref="IArgument{C}"/>.
+/// </para>
+/// </summary>
 public record ArgumentMultiplicity
 {
+    /// <summary>
+    /// Represents such a multiplicity in which the number of parts and argument should have is one
+    /// number known in advance.
+    /// </summary>
+    /// <param name="IsRequired">Allows for the omitting of the argument.</param>
     public record SpecificCount(int Number, bool IsRequired) : ArgumentMultiplicity();
+    /// <summary>
+    /// Represents such a multiplicity in which all the following argument parts belong to the given
+    /// plain argument. 
+    /// </summary>
+    /// <param name="MinimumNumberOfArguments">Allow for the specification of minimum number of parts required
+    /// which should then be enforced by the parser.</param>
     public record AllThatFollow(int MinimumNumberOfArguments = 0) : ArgumentMultiplicity();
     private ArgumentMultiplicity() { }
 }
 
 public interface IArgument<C>
 {
+    /// <summary>
+    /// Name of the argument as it should appear in help write-up and debug messages.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Description of the argument as it should appear in help write-up.
+    /// </summary>
     public string Description { get; set; }
     /// <summary>
     /// Value placeholder will be used in synopsis e.g. program [options] <value-placeholder> ...
     /// </summary>
     public string ValuePlaceholder { get; set; }
+    /// <summary>
+    /// Codifies information about what number or range of numbers of parts for this argument is to be expected.
+    /// </summary>
     public ArgumentMultiplicity Multiplicity { get; set; }
     internal void Process(C config, string value);
 }
@@ -67,8 +55,15 @@ public sealed record Argument<C, V> : IArgument<C>
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
     public string ValuePlaceholder { get; set; } = "<arg>";
+    /// <summary>
+    /// Function to convert the argument value parsed as a string from the input to the target type.
+    /// </summary>
     public Func<string, V> Converter { get; set; } = (strVal) => default;
+    /// <summary>
+    /// Action to be carried out upon parsing of the argument in the input.
+    /// </summary>
     public Action<C, V> Action { get; set; } = (conf, val) => { };
+    
     public ArgumentMultiplicity Multiplicity { get; set; } = new ArgumentMultiplicity.SpecificCount(Number: 1, IsRequired: true);
     void IArgument<C>.Process(C config, string value)
     {
@@ -86,11 +81,15 @@ public interface IOption<C>
     /// One option can represent both long and short options
     /// </summary>
     public string[]? Names { get; set; }
-    
+
     /// <summary>
-    /// Description will be used in help message.
+    /// Description of the option as it should appear in help write-up.
     /// </summary>
     public string Description { get; set; }
+    /// <summary>
+    /// If false, the parser will produce an error state upon finishing parsing without encountering
+    /// the option.
+    /// </summary>
     public bool IsRequired { get; set; }
     /// <summary>
     /// Value used in help message as placeholder for value.
@@ -101,13 +100,24 @@ public interface IOption<C>
     internal void Process(C config, string value);
 }
 
+/// <summary>
+/// Models a parser option which is to accept a value provided by the user.
+/// </summary>
+/// <typeparam name="C">The cofiguration context type for the given parser, see <see cref="Parser{C}"/></typeparam>
+/// <typeparam name="V">Type of the value the provided string is to be converted to. /// </typeparam>
 public sealed record Option<C, V> : IOption<C>
 {
     public string[]? Names { get; set; }
     public string ValuePlaceHolder { get; set; } = "<value>";
     public string Description { get; set; } = "";
+    /// <summary>
+    /// Action to be carried out upon parsing and conversion of the option from the input.
+    /// </summary>
     public Action<C, V> Action { get; set; } = (conf, val) => { };
     public bool IsRequired { get; set; } = false;
+    /// <summary>
+    /// Function to convert the argument value parsed as a string from the input to the target type.
+    /// </summary>
     public Func<string, V> Converter { get; set; } = (strVal) => default;
     void IOption<C>.Process(C config, string value)
     {
@@ -116,32 +126,79 @@ public sealed record Option<C, V> : IOption<C>
     }
 }
 
+/// <summary>
+/// A convenience class used for speedy creation of common option types.
+/// </summary>
 public static class OptionFactory
 {
+    /// <summary>
+    /// Creates a simple string-valued option for a <see cref="Parser{C}"/> 
+    /// with config context <typeparamref name="C"/>.
+    /// </summary>
     public static Option<C, string> CreateStringOption<C>()
     {
         return new Option<C, string>() { Converter = (string s) => { return s; } };
     }
+
+    /// <summary>
+    /// Creates a option which accepts a list of values convertible to type <typeparamref name="T"/>
+    /// by the provided convertor function <paramref name="convertor"/>.
+    /// </summary>
+    /// <typeparam name="C">Config context type of the parent parser.</typeparam>
+    /// <typeparam name="T">Type of the individual values listed in the option value.</typeparam>
+    /// <param name="convertor">Function to convert the individiual values from string to intended type.</param>
+    /// <param name="separator">Separator of the individual values in the option value</param>
     public static Option<C, List<T>> CreateListOption<C, T>(Func<string, T> convertor, char separator = ',')
     {
         return new Option<C, List<T>>() { Converter = (string s) => { return s.Split(separator).Select(x => convertor(x)).ToList(); } };
     }
 }
 
+/// <summary>
+/// Represent a flag, an option that is always only either ON or OFF, i.e. without any further
+/// value provided by the user.
+/// </summary>
+/// <typeparam name="C">The cofiguration context type for the given parser, see <see cref="Parser{C}"/> </typeparam>
 public sealed record Flag<C>
 {
+    /// <summary>
+    /// Names the options as they will be parsed by the parser.
+    /// Names of long options are prefixed with two dashes '--'
+    /// Names of short options are prefixed with one dash '-'
+    /// One option can represent both long and short options
+    /// </summary>
     public string[]? Names { get; set; }
+    /// <summary>
+    /// Description of the flag-like option as it should appear in help write-up.
+    /// </summary>
     public string Description { get; set; } = "";
+    /// <summary>
+    /// Action to be carried out upon parsing of the flag in the input.
+    /// </summary>
     public Action<C> Action { get; set; } = (conf) => { };
 }
 
+/// <summary>
+/// An object which is used to create help write-ups for parsers, the message one usually sees
+/// when a command is passed the --help flag.
+/// </summary>
+/// <typeparam name="T">The cofiguration context type for the given parser, see <see cref="Parser{C}"/></typeparam>
 public interface IParserHelpFormatter<T>
 {
+    /// <summary>
+    /// Prints a help write-up to the provided <see cref="TextWriter"/> from the information
+    /// gathered from the provided <paramref name="parser"/>.
+    /// </summary>
     public abstract void PrintHelp(Parser<T> parser, TextWriter writer);
 }
 
+/// <summary>
+/// An implementation of <see cref="IParserHelpFormatter{T}"/> that provides basic
+/// help formatting which includes all information about options and arguments
+/// </summary>
 public sealed class DefaultHelpFormatter<T> : IParserHelpFormatter<T>
 {
+
     public void PrintHelp(Parser<T> parser, TextWriter writer)
     {
         System.Console.WriteLine(parser.Name);
@@ -274,12 +331,16 @@ public record Parser<C> : IParser
     /// <summary>
     /// Returns a dictionary of all atached subparsers with keys being names of the 
     /// commands attached to the parsers.
+    /// <para>See <see cref="AddSubparser(string, IParser)"</see></para>
     /// </summary>
     public IReadOnlyDictionary<string, IParser> GetSubparsers() => SubParsers;
     /// <summary>
     /// Attach a subparser to this parser as a subcommand <paramref name="command"/>. 
     /// The subparser is then triggered and used to parse the rest of the input after the command
     /// token is found in input.
+    /// <para>
+    /// See <see cref="Parser{C}"/>.
+    /// </para>
     /// </summary>
     /// <param name="command">A string command with which the subparser will be triggred. May not contain spaces</param>
     /// <param name="commandParser">Parser to attach.</param>
@@ -300,8 +361,8 @@ public record Parser<C> : IParser
     /// <summary>
     /// Attach a flag-like option to the parser.
     /// <para>To attach more <see cref="Flag{C}"/> objects at once, see <see cref="AddFlags(Flag{C}[])"/></para>
+    /// <para>
     /// </summary>
-    /// <seealso cref="Flag{C}"/>
     /// <returns>The parent parser as to allow for chaining of calls and fluent syntax.</returns>
     public Parser<C> AddFlag(Flag<C> flag)
     {
@@ -311,9 +372,11 @@ public record Parser<C> : IParser
     }
     /// <summary>
     /// Attach one or more flag-like options to the parser.
-    /// 
+    /// <para>
+    /// See also
+    /// </para>
+    /// <seealso cref="Flag{C}"/>.
     /// </summary>
-    /// <seealso cref="Flag{C}"/>
     /// <returns>The parent parser as to allow for chaining of calls and fluent syntax.</returns>
     public Parser<C> AddFlags(params Flag<C>[] flags) { return this; }
 
@@ -377,42 +440,67 @@ public record Parser<C> : IParser
     /// <returns>The parent parser as to allow for chaining of calls and fluent syntax.</returns>
     public Parser<C> AddArguments(params IArgument<C>[] argument) { return this; }
 
+    /// <summary>
+    /// Creates parser with config context <typeparamref name="C"/>.
+    /// </summary>
+    /// <param name="config">Instance of config context to be used.</param>
     public Parser(C config)
     {
         Config = config;
     }
+
+    /// <summary>
+    /// Creates parser with config context <typeparamref name="C"/>.
+    /// </summary>
+    /// <param name="configFactory">Factory method to be used to instantiate the config object when
+    /// the parser is used.</param>
     public Parser(Func<C> configFactory)
     {
         ConfigFactory = configFactory;
     }
 
+    /// <summary>
+    /// Prints a formatted help message to the provided output <c>TextWriter</c> with information
+    /// about parser usage, arguments, options, and subcommand parsers as formatted by the provided <paramref name="formatter"/>
+    /// </summary>
     public void PrintHelp(IParserHelpFormatter<C> formatter, TextWriter writer) => formatter.PrintHelp(this, writer);
+    /// <summary>
+    /// Prints a formatted help message to the provided output <c>TextWriter</c> with information
+    /// about parser usage, arguments, options, and subcommand parsers as formatted by <see cref="DefaultHelpFormatter{T}"/>
+    /// </summary>
     public void PrintHelp(TextWriter writer) => PrintHelp(new DefaultHelpFormatter<C>(), writer);
+    /// <summary>
+    /// Prints a formatted help message to the console with information
+    /// about parser usage, arguments, options, and subcommand parsers as formatted by the provided <paramref name="formatter"/>.
+    /// </summary>
     public void PrintHelp(IParserHelpFormatter<C> formatter) => PrintHelp(formatter, System.Console.Out);
+    /// <summary>
+    /// Prints a formatted help message to the console with information
+    /// about parser usage, arguments, options, and subcommand parsers as formatted by <see cref="DefaultHelpFormatter{T}"/>
+    /// </summary>
     public void PrintHelp() => PrintHelp(new DefaultHelpFormatter<C>(), System.Console.Out);
-
-    public void ParseAndRun(string[] args) => ParseAndRun(args, true, Run);
     public void Parse(string[] args) => ParseAndRun(args, true, (_) => { });
+    public void ParseAndRun(string[] args) => ParseAndRun(args, true, this.Run);
     void ParseAndRun(string[] args, bool isRoot, Action<C> localRun)
     {
         throw new NotImplementedException();
-        if (isRoot)
-        {
-            // determine command and then find parser and run ParseAndRun on
-            // that parser with the commands removed from the arguments
-            //ParseAndRun(..., false);
-        }
-        else
-        {
-            if (Config is null)
-            {
-                if (ConfigFactory is null)
-                    throw new Exception("Config and ConfigFactory are both null. This should never happen");
-                Config = ConfigFactory();
-            }
-            // TODO: parse remaining arguments
+        //if (isRoot)
+        //{
+        //    // determine command and then find parser and run ParseAndRun on
+        //    // that parser with the commands removed from the arguments
+        //    //ParseAndRun(..., false);
+        //}
+        //else
+        //{
+        //    if (Config is null)
+        //    {
+        //        if (ConfigFactory is null)
+        //            throw new Exception("Config and ConfigFactory are both null. This should never happen");
+        //        Config = ConfigFactory();
+        //    }
+        //    // TODO: parse remaining arguments
 
-            localRun(Config);
-        }
+        //    localRun(Config);
+        //}
     }
 }
