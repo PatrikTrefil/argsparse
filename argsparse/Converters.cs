37b55327612adaf0e21f5442b8671439beb69ce3
zdenecek <zdnek.tomis@gmail.com>
2023-03-23 20:19:06 +0100

    Add enum converters
    (cherry picked from commit a4472672da767b72c7ab034966dbd04e36944c76)

--- argsparse/Converters.cs
+++ argsparse/Converters.cs
@@ -6,6 +6,7 @@ namespace Argparse;
 
 public static class ConverterFactory
 {
+    
     public static Func<string, string> CreateStringConverter() => (string s) => { return s; };
     public static Func<string, List<T>> CreateListConverter<T>(Func<string, T> converter, char separator = ',')
     {
@@ -34,5 +35,41 @@ public static class ConverterFactory
 
                 return value;
             };
+    }
+
+    /// <summary>
+    /// Creates a converter for an enumeration type.
+    /// </summary>
+    /// <typeparam name="T">The enumeration type, has to be Enum</typeparam>
+    /// <param name="caseSensitive">If true, the converter match enum values no matter the case</param>
+    /// <param name="mapping">A dictionary that maps strings to enum values. If a string is not found in the dictionary, the converter will try to parse the string as an enum value. If no mapping is provided, the convertor will try to match enum names.</param>
+    /// <description>
+    /// The converter will try to match the input string to the enum values in the following order:
+    /// 1. If a mapping is provided, the converter will try to match the input string to the keys in the mapping.
+    /// 2. If the input string is a valid enum value, the converter will return the corresponding enum value.
+    /// 3. If above fails, <see cref="ArgumentException"/> will be thrown.
+    /// </description>
+    public static Func<string, T> CreateEnumerationConverter<T>(bool caseSensitive = true, Dictionary<string, T>? mapping = null)
+        where T : Enum
+    {        
+        return null;
+    }
+
+    /// <summary>
+    /// Creates a converter for an enumeration type.
+    /// </summary>
+    /// <typeparam name="T">The enumeration type, has to be Enum</typeparam>
+    /// <param name="caseSensitive">If true, the converter match enum values no matter the case</param>
+    /// <param name="mapping">A dictionary that maps strings to enum values. If a string is not found in the dictionary, the converter will try to parse the string as an enum value. If no mapping is provided, the convertor will try to match enum names.</param>
+    /// <description>
+    /// The converter will try to match the input string to the enum values in the following order:
+    /// 1. If a mapping is provided, the converter will try to match the input string to the keys in the mapping.
+    /// 2. If the input string is a valid enum value, the converter will return the corresponding enum value.
+    /// 3. If above fails, <see cref="ArgumentException"/> will be thrown.
+    /// </description>
+    public static Func<string, T> CreateEnumerationConverter<T>(bool caseSensitive = true, Dictionary<string[], T>? mapping = null)
+        where T : Enum
+    {        
+        return null;
     }
 }
