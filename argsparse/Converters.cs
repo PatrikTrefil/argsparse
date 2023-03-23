1e95c7cb629e5c591dd345aae17fd7d480c34736
zdenecek <zdnek.tomis@gmail.com>
2023-03-23 20:11:31 +0100

    fix int converter params naming, add comments

--- argsparse/Converters.cs
+++ argsparse/Converters.cs
@@ -13,24 +13,26 @@ public static class ConverterFactory
     }
     public static Func<string, bool> CreateBoolConverter()
     {
+        // this implementation is only a demo, it is not complete, do not base tests on it
         return (string s) =>
             {
                 if (s == "true") return true;
+                // more logic here
                 return false;
             };
     }
-    public static Func<string, int> CreateIntConverter(int minValue = int.MinValue, int maxValue = int.MaxValue)
+    public static Func<string, int> CreateIntConverter(int minValueInclusive = int.MinValue, int maxValueInclusive = int.MaxValue)
     {
+        // this implementation is only a demo, it is not complete, do not base tests on it
         return (string s) =>
             {
                 int value = int.Parse(s);
-                if (value < minValue || value > maxValue)
+                if (value <= minValueInclusive || value >= maxValueInclusive)
                     throw new ArgumentException(
-                        $"Value {value} is out of range [{minValue}, {maxValue}]"
+                        $"Value {value} is out of range [{minValueInclusive}, {maxValueInclusive}]"
                         );
 
                 return value;
             };
-
     }
 }
