3544bb1d90cfb08b73ccf6848cffed86089b9037
Patrik Trefil <patrik.trefil@gmail.com>
2023-04-26 14:37:04 +0200

    rm unused imports

--- tests2/ConfigTests/ConfigParserTests.cs
+++ tests2/ConfigTests/ConfigParserTests.cs
@@ -1,10 +1,4 @@
 using Argparse;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestsArgparseAPI.ConfigTests
 {
